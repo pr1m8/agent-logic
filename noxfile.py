357def500135f3948051dc81160825fe1321f660
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-04-28 15:01:55 -0400

    Enhance noxfile.py by adding a new session for auto-rebuilding Sphinx documentation on file changes and updating the existing documentation build process to use 'sphinx-autobuild' for improved efficiency.

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,27 @@
 import nox
 
+
 @nox.session(name="docs", python="3.12")
 def build_docs(session: nox.Session) -> None:
+    """Build the Sphinx documentation."""
+    session.install("poetry")
     session.run("poetry", "install", "--with", "docs", external=True)
-    session.run("sphinx-build", "-b", "html", "docs/source", "docs/build/html")
+    session.run(
+        "sphinx-build",
+        "-b",
+        "html",
+        "docs/source",
+        "docs/build/html",
+    )
+
+
+@nox.session(name="docs-autobuild", python="3.12")
+def autobuild_docs(session: nox.Session) -> None:
+    """Auto-rebuild Sphinx docs on file changes."""
+    session.install("poetry")
+    session.run("poetry", "install", "--with", "docs", external=True)
+    session.run(
+        "sphinx-autobuild",
+        "docs/source",
+        "docs/build/html",
+    )
