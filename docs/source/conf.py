dd5a131cc9f1b82f480cec8fbc08b3924b4d5d0a
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-04-28 14:43:33 -0400

    Add initial Sphinx configuration and index files for project documentation, including project metadata, extensions, and structure for API reference.

---
+++ docs/source/conf.py
@@ -0,0 +1,69 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'agent-logic'
+copyright = '2025, William R. Astley (Pr1m8)'
+author = 'William R. Astley (Pr1m8)'
+release = '0.1.0'
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Add any Sphinx extension module names here
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # for Google and NumPy docstrings
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_inline_tabs",
+    "sphinx_tabs.tabs",
+    "myst_parser",
+    "sphinxcontrib.mermaid",
+    "sphinxcontrib.youtube",
+    "sphinxext.opengraph",
+    "sphinx_rtd_theme",  # optional backup
+    "sphinx_press_theme",  # optional backup
+    "sphinx_needs",  # advanced requirements diagrams
+    "sphinx_sitemap",  # sitemap.xml for SEO
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+autosummary_generate = True
+autodoc_typehints = "description"  # clean type hints in docstrings
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "private-members": False,
+    "show-inheritance": True,
+}
+todo_include_todos = True
+
+# Myst-parser settings if using Markdown
+myst_enable_extensions = [
+    "deflist",
+    "fieldlist",
+    "html_image",
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+Welcome to Your Project
+========================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+API Reference
+=============
+
+.. autosummary::
+   :toctree: generated
+   :recursive:
+
+   logic  # <-- name of your src module
